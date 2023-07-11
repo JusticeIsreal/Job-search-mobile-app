82a361f155ebc19f034e8603ed82591b5dbf1d2f
Justice Isreal <justiceyba@gmail.com>
2023-07-11 19:40:52 +0100

    popular job component completed

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,14 +1,51 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
+import { useRouter } from "expo-router";
 
-import styles from './nearbyjobs.style'
+import styles from "./nearbyjobs.style";
+import { COLORS } from "../../../constants";
 
+// COMPONENTS
+import NearbyjobsCard from "../../common/cards/nearby/NearbyJobCard";
+
+// API CALL
+import useFetch from "../../../../job-search-app/hook/useFetch";
 const Nearbyjobs = () => {
-  return (
-    <View>
-      <Text>Nearbyjobs</Text>
-    </View>
-  )
-}
+  const router = useRouter();
 
-export default Nearbyjobs
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React developer",
+    num_pages: 1,
+  });
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Nearby Jobs</Text>
+
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" color={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          // <FlatList
+          //   data={data}
+          //   keyExtractor={(item) => item?.job_id}
+          //   renderItem={({ item }) => <PopularJobCard item={item} />}
+          //   contentContainerStyle={{ columnGap: SIZES.medium }}
+          //   horizontal
+          // />
+          data?.map((job) => (
+            <NearbyjobsCard job={job} key={`nearby-job-${job?.job_id}`} />
+          ))
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Nearbyjobs;
