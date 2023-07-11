a33c42b244946f44a5ff9d183914ca43d0d6b0bf
Justice Isreal <justiceyba@gmail.com>
2023-07-11 15:03:22 +0100

    search bar design completed

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
+import { icons, SIZES } from "../../../constants";
+
+import styles from "./welcome.style";
+
+const Welcome = () => {
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Justice</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChangeText={() => {}}
+            placeholder="Whare are you loooking for?"
+          />
+        </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
